f2909f8a870a03ae4ed4a60061829e855fd8d737
Surya sakala <18suryateja@gmail.com>
2025-07-06 16:44:33 +0530

    Create sandbox-aft01 Control Tower account

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,22 +1,31 @@
-module "aft1" {
-    source = "./modules/aft-account-request"
-    control_tower_parameters = {
-        AccountEmail = "sakalak+aft1@amazon.com"
-        AccountName = "aft1"
-        ManagedOrganizationalUnit = "Sandbox (ou-jl8v-kolgi4le)"
-        SSOUserEmail = "sakalak+aft1@amazon.com"
-        SSOUserFirstName = "aft"
-        SSOUserLastName = "1"
-    }
-    account_tags = {
-        "Learn Tutorial" = "AFT1"
-    }
-    change_management_parameters = {
-        change_requested_by = "HashiCorp Learn"
-        change_reason = "Learn AWS Control Tower Account Factory for Terraform"
-    }
-    custom_fields = {
-        group = "non-prod"
-    }
-    account_customizations_name = "sandbox"
+# Copyright Amazon.com, Inc. or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+module "sandbox_account_aft1" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail = "sakalak+aft01@amazon.com"
+    AccountName  = "sandbox-account-01"
+    ManagedOrganizationalUnit = "Sandbox (ou-jl8v-kolgi4le)"
+    SSOUserEmail     = "sakalak+aft01@amazon.com"
+    SSOUserFirstName = "aft"
+    SSOUserLastName  = "01"
+  }
+
+  account_tags = {
+    "ABC:Project"     = "123456"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Surya Teja"
+    change_reason       = "testing the account vending process"
+  }
+
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
+
+  account_customizations_name = "sandbox-aft1"
 }
