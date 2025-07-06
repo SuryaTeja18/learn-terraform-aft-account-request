5d9d558730e74ff460380eb417c7360030074435
Surya sakala <18suryateja@gmail.com>
2025-07-06 15:59:50 +0530

    Create sandbox-aft1 Control Tower account

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,22 @@
-module "sandbox" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
-  }
-
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "sandbox"
+module "aft1" {
+    source = "./modules/aft-account-request"
+    control_tower_parameters = {
+        AccountEmail = "sakalak+aft1@amazon.com"
+        AccountName = "aft1"
+        ManagedOrganizationalUnit = "Sandbox (ou-jl8v-kolgi4le)"
+        SSOUserEmail = "sakalak+aft1@amazon.com"
+        SSOUserFirstName = "aft"
+        SSOUserLastName = "1"
+    }
+    account_tags = {
+        "Learn Tutorial" = "AFT1"
+    }
+    change_management_parameters = {
+        change_requested_by = "HashiCorp Learn"
+        change_reason = "Learn AWS Control Tower Account Factory for Terraform"
+    }
+    custom_fields = {
+        group = "non-prod"
+    }
+    account_customizations_name = "sandbox"
 }
